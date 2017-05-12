8d6a4d3b3e12cf74e0a9fafa2b4ed0ef3ace3072
Youssef Rizkk <youssefarizk@gmail.com>
2017-05-12 16:47:51 +0100

    My first commit

---
+++ EmotionDemo/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EmotionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.output = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.emotionButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.output.Location = new System.Drawing.Point(333, 12);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(197, 218);
+            this.output.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(283, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureButton
+            // 
+            this.pictureButton.Location = new System.Drawing.Point(12, 260);
+            this.pictureButton.Name = "pictureButton";
+            this.pictureButton.Size = new System.Drawing.Size(71, 43);
+            this.pictureButton.TabIndex = 2;
+            this.pictureButton.Text = "Upload a picture";
+            this.pictureButton.UseVisualStyleBackColor = true;
+            this.pictureButton.Click += new System.EventHandler(this.pictureButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // emotionButton
+            // 
+            this.emotionButton.Location = new System.Drawing.Point(371, 260);
+            this.emotionButton.Name = "emotionButton";
+            this.emotionButton.Size = new System.Drawing.Size(136, 43);
+            this.emotionButton.TabIndex = 3;
+            this.emotionButton.Text = "Show Emotion";
+            this.emotionButton.UseVisualStyleBackColor = true;
+            this.emotionButton.Click += new System.EventHandler(this.emotionButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.emotionButton);
+            this.Controls.Add(this.pictureButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Emotion Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button pictureButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button emotionButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EmotionDemo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+  
+
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+
+namespace EmotionDemo
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        const string APIKey = "e0d17a705f3647f9b451cebc8bcfb8a2";
+
+        EmotionServiceClient emotionClient = new EmotionServiceClient(APIKey);
+        Emotion[] emotionResult;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureButton_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private async void emotionButton_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Stream imageFileStream = File.OpenRead(pictureBox1.ImageLocation);
+                emotionResult = await emotionClient.RecognizeAsync(imageFileStream);
+
+                if (emotionResult != null)
+                {
+                    var score = emotionResult[0].Scores;
+                    counter++;
+                    output.Text = "The emotions for the first face detected are:" +
+                        "\r\n Anger: " + score.Anger +
+                        "\r\n Contempt: " + score.Contempt +
+                        "\r\n Disgust: " + score.Disgust +
+                        "\r\n Fear: " + score.Fear +
+                        "\r\n Happiness: " + score.Happiness +
+                        "\r\n Neutral: " + score.Neutral +
+                        "\r\n Sadness: " + score.Sadness +
+                        "\r\n Surprise: " + score.Surprise
+                        + "\n This is the " +counter + "th iteration"
+                        ;
+                }
+            }
+            catch
+            {
+                output.Text += "Error accessing the Picture at location: " + pictureBox1.ImageLocation;
+            }
+        }
+    }
+}
